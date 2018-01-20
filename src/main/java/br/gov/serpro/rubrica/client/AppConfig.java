1f000c7f2dff682c72c8ab70b1393d58af9310ab
estevaocm <estevao@gmail.com>
2018-01-20 00:21:28 -0300

    Copyright/license

--- src/main/java/br/gov/serpro/rubrica/client/AppConfig.java
+++ src/main/java/br/gov/serpro/rubrica/client/AppConfig.java
@@ -1,38 +1,34 @@
 /*
- * Demoiselle Framework
- * Copyright (C) 2010 SERPRO
+ * RubricaClient
+ * Copyright (C) 2018 SERPRO
  * ----------------------------------------------------------------------------
- * This file is part of Demoiselle Framework.
- * 
- * Demoiselle Framework is free software; you can redistribute it and/or
+ * RubricaClient is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License version 3
  * as published by the Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License version 3
  * along with this program; if not,  see <http://www.gnu.org/licenses/>
  * or write to the Free Software Foundation, Inc., 51 Franklin Street,
  * Fifth Floor, Boston, MA  02110-1301, USA.
  * ----------------------------------------------------------------------------
- * Este arquivo é parte do Framework Demoiselle.
- * 
- * O Framework Demoiselle é um software livre; você pode redistribuí-lo e/ou
- * modificá-lo dentro dos termos da GNU LGPL versão 3 como publicada pela Fundação
+ * RubricaClient é um software livre; você pode redistribuí-lo e/ou
+ * modificá-lo dentro dos termos da GNU LGPL versão 3 publicada pela Fundação
  * do Software Livre (FSF).
- * 
- * Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
- * GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
- * APLICAÇÃO EM PARTICULAR. Veja a Licença Pública Geral GNU/LGPL em português
- * para maiores detalhes.
- * 
+ *
+ * Este programa é distribuído com a esperança que possa ser útil, mas SEM GARANTIA
+ * ALGUMA; sem sequer uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou
+ * APLICAÇÃO ESPECÍFICA. Veja a Licença Pública Geral GNU/LGPL em português
+ * para mais detalhes.
+ *
  * Você deve ter recebido uma cópia da GNU LGPL versão 3, sob o título
- * "LICENCA.txt", junto com esse programa. Se não, acesse <http://www.gnu.org/licenses/>
- * ou escreva para a Fundação do Software Livre (FSF) Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
+ * "LICENCA.txt", junto com esse programa. Caso contrário, acesse 
+ * <http://www.gnu.org/licenses/> ou escreva para a Fundação do Software Livre (FSF) 
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02111-1301, USA.
  */
 package br.gov.serpro.rubrica.client;
 
